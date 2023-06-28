2a08f62b452f497aa77dd4dffc2ee964ae380051
Roy <113089421+Roy42022p@users.noreply.github.com>
2023-06-28 10:23:43 +0300

    Add files via upload

---
+++ Example/C#/Program.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите фамилию: ");
+        string фамилия = Console.ReadLine();
+
+        Console.Write("Введите оценку: ");
+        int оценка = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Поздравляю, {фамилия}! У Вас автоматом {оценка} за экзамен. ");
+
+    }
+}
